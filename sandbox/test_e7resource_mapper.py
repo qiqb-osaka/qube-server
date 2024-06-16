d927eb35966fce549559c323118833ebbaeb76ff
Toshio Mori <forest1040>
2024-06-16 10:26:59 +0900

    ResourceMapper

--- sandbox/test_e7resource_mapper.py
+++ sandbox/test_e7resource_mapper.py
@@ -6,5 +6,12 @@ box = Quel1Box.create(
     boxtype=Quel1BoxType.QuBE_OU_TypeA,
 )
 
+group, line = 0, 0
+
 rmap = Quel1E7ResourceMapper(box.css, box.wss)
-print(rmap)
+#print(rmap)
+channels = box.css._get_channels_of_line(group, line)
+print("channels: ", channels)
+channel = channels[0]
+aws_ch = rmap.get_awg_of_channel(group, line, channel)
+print("aws_ch: ", aws_ch)
